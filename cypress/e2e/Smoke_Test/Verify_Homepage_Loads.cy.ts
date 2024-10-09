0dfd3889d4754a19f4b3a5e9b027e209abb5d786
Grudilov24 <dmytrohrudilov@gmail.com>
2024-10-09 15:24:48 +0300

    add several tests
    - Verify Homepage Loads
    - Product Search
    -  Add a Product to the Cart

--- cypress/e2e/Smoke_Test/Verify_Homepage_Loads.cy.ts
+++ cypress/e2e/Smoke_Test/Verify_Homepage_Loads.cy.ts
@@ -1,26 +1,29 @@
-import selectors from "../..//fixtures///VerifyHomepageLoadsSelectors.json"
 describe('Verify Homepage Loads Successfully', () => {
   
   // Before the test begins, navigate to the homepage
   before(() => {
     cy.visit('');
-  });
+  })
 
   it('Should display the store logo', () => {
 
   // Verify that key elevey is visible at the top of the page
   //Verify logo
-    cy.get('img[alt="Демо магазин OpenCart"]')
+    cy.findAllByAltText('Демо магазин OpenCart')
     .should('exist')
-    .should('be.visible');
+    .should('be.visible')
 
   //Verify navigation bar
-    cy.get('.navbar').should('exist').and('be.visible');
+    cy.get('.navbar')
+    .should('exist')
+    .and('be.visible')
 
   // Verify that the search bar is visible
-    cy.findAllByPlaceholderText('Пошук').should('be.visible');
+    cy.findAllByPlaceholderText('Пошук')
+    .should('be.visible')
 
     // Verify that the featured products section is present
-    cy.get('#content .product-layout').should('have.length.greaterThan', 0);
-  });
-});
+    cy.get('#content .product-layout')
+    .should('have.length.greaterThan', 0)
+  })
+})
