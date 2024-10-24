013b33f8dc0feba4722fcbff6acabe437ebd23d6
Grudilov24 <116968051+Grudilov24@users.noreply.github.com>
2024-10-24 11:31:25 +0300

    Update CheckoutProcess.cy.ts

--- cypress/e2e/smokeTest/CheckoutProcess.cy.ts
+++ cypress/e2e/smokeTest/CheckoutProcess.cy.ts
@@ -2,7 +2,7 @@ import searchItemSelectors from "../../fixtures/addProductToTheCartSelectors.jso
 import logInSelectors from "../../fixtures/userLogInSelectors.json"
 import checkoutSelectors from "..//../fixtures/checkoutProcessSelectors.json"
 
-describe('Adding a Product to the Cart', () => {
+describe('Testing checkout process', () => {
 
     let productName = 'iPhone'
 
@@ -79,31 +79,28 @@ describe('Adding a Product to the Cart', () => {
     it('Steps for ordering using registration (default) data', () => {
 
         // Step 1
-        cy.findByText('Крок 1: Варіанти оформлення').should('be.visible');
-        cy.findByRole('button', { name: /Продовжити/i }).click();
+        cy.findByText('Крок 1: Варіанти оформлення').should('be.visible')
+        cy.findByRole('button', { name: /Продовжити/i }).click()
 
         // Step 2
-        cy.findByText('Крок 2: Деталі рахунку').should('be.visible');
-        cy.findByRole('button', { name: /Продовжити/i }).click();
+        cy.findByText('Крок 2: Деталі рахунку').should('be.visible')
+        cy.findByRole('button', { name: /Продовжити/i }).click()
 
         // Step 3
-        cy.findByText('Крок 3: Деталі доставки').should('be.visible');
-        cy.findByRole('button', { name: /Продовжити/i }).click();
+        cy.findByText('Крок 3: Деталі доставки').should('be.visible')
+        cy.findByRole('button', { name: /Продовжити/i }).click()
 
         // Step 4
-        cy.findByText('Крок 4: Спосіб доставки').should('be.visible');
-        cy.findByRole('button', { name: /Продовжити/i }).click();
+        cy.findByText('Крок 4: Спосіб доставки').should('be.visible')
+        cy.findByRole('button', { name: /Продовжити/i }).click()
 
         // Step 5 - Click on checkbox
-        cy.findByText('Крок 5: Спосіб оплати').should('be.visible');
+        cy.findByText('Крок 5: Спосіб оплати').should('be.visible')
         cy.findByRole('checkbox').check(); // Use the role of the checkbox
-        cy.findByRole('button', { name: /Продовжити/i }).click();
+        cy.findByRole('button', { name: /Продовжити/i }).click()
 
-        cy.findByText('Крок 6: Підтвердження замовлення').should('be.visible');
-        cy.findByRole('button', { name: /Продовжити/i }).click();
-        // Verify that the order was successfully completed
-
-
+        cy.findByText('Крок 6: Підтвердження замовлення').should('be.visible')
+        cy.findByRole('button', { name: /Продовжити/i }).click()
 
         // Comparison search item and item in order table
         cy.get(checkoutSelectors.orderTable)
@@ -124,6 +121,5 @@ describe('Adding a Product to the Cart', () => {
         cy.findAllByText('Ви успішно оформили замовлення!')
             .should('exist')
             .should('be.visible')
-
     })
 })
